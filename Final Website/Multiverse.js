5a5735ff84d3fc8e1539b6acd9547686d6d5c7db
ItsJony42 <oo23xr@brocku.ca>
2024-12-15 18:10:36 -0500

    s

---
+++ Final Website/Multiverse.js
@@ -0,0 +1,68 @@
+function addWV(){
+  var x = document.getElementById("WV").name;
+alert ("Wandavision is the first tv show in the MCU, and it follows a reality that Wanda created, where her life goes exactly how she wanted it to be - before Vision's death.");
+}
+
+function addFal(){
+  var x = document.getElementById("Fal").name;
+alert ("This is the second tv show in the MCU, and the main character is Falcon, as he struggles to take the mantle of Captain America and to help keep world order after the Blip - the period where people were snapped away and then snapped back.");
+}
+
+function addLoki(){
+  var x = document.getElementById("Loki").name;
+alert ("After Endgame, Loki escapes his timeline while the Avengers are back in time, which leads to him having his own show and purpose in the Multiverse Saga.");
+}
+
+function addBW(){
+  var x = document.getElementById("BW").name;
+alert ("This movie serves as Black Widow's own adventure between Civil War and Infinity War, where she reunites with her family and deal with Black Widow's previous trauma.");
+}
+
+function addShang(){
+  var x = document.getElementById("Shang").name;
+alert ("After Iron Man 3's appropriation of the Mandarin, this movie highlights the actual villain. Shang-Chi, the Mandarin's son, has to face his father since he finds him in America after he ran away, forcing him to deal with the Mandarin and his evil plans.");
+}
+
+function addEtern(){
+  var x = document.getElementById("Eternals").name;
+alert ("The Eternals are a group of perfect beings created by a Celestial named Arishem, and they go from planet to planet to nurture it so another Celestial can be born from inside. However, this time, the Eternals are on Earth, and they come to realize that Earth is too good of a planet to fall to this plan, so they help protect it from the Emergence - the process of a new Celestial being created.");
+}
+function addHawk(){
+  var x = document.getElementById("Hawk").name;
+alert ("This tv show follows the life of Hawkeye after Endgame, where he has to deal with his own problems instead of with the Avengers. Here he meets Kate Bishop, a new sidekick of his that he trains, and the two work together to defeat the villain Kingpin.");
+}
+
+function addSpidey3(){
+  var x = document.getElementById("Spidey3").name;
+alert ("The end of the Spider-Man trilogy is what first starts off the Multiverse aspect of the saga. After Peter's identity is revealed, he goes to Doctor Strange to help people forget his identity. He ends up messing up the spell, and starts brinigng in people from other universes, such as the other Spider-Men and their villains from the other franchises.");
+}
+
+function addMK(){
+  var x = document.getElementById("MK").name;
+alert ("Moon Knight is a character who becomes an avatar for the Egyptian God Khonshu, and Khonshu manipulates Moon Knight to do his tasks because of his split personality.");
+}
+
+function addDoc2(){
+  var x = document.getElementById("Doc2").name;
+alert ("After the events of Wandavision, Wanda becomes evil and does whatever it takes to return to her fake reality. Doctor Strange takes the responsbility of stopping her search across the multiverse to find a reality where her life is perfect.");
+}
+
+function addMs(){
+  var x = document.getElementById("Ms").name;
+alert ("Captain Marvel left an impact on society during Endgame, creating several fans of her such as Kamala Khan, the protagonist of the series. The series follows Kamala on her own journey to become a hero, just like Captain Marvel.");
+}
+
+function addThor4(){
+  var x = document.getElementById("Thor4").name;
+alert ("Following Endgame, Thor tags along with the Guardians for a while. He splits off from them in this movie, and embarks on his own journey as he faces a new threat - Gorr the God Butcher.");
+}
+
+function addShe(){
+  var x = document.getElementById("She").name;
+alert ("In a car accident with Bruce, Jennifer Walters and him mix blood, giving Jennifer a new Hulk form. This show follows her as she faces this new form while also continuing to be a lawyer.");
+}
+
+function addBP2(){
+  var x = document.getElementById("BP2").name;
+alert ("The final movie of Phase 4 is the second Black Panther. Unfortunately, the actor Chadwick Boseman passed away. To move on with the MCU storyline, the directors had T'Challa's sister Shuri become the new Black Panther, where she has to lead her people to fight against another secret civilization led by Namor.");
+}
